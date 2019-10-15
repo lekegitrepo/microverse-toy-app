266d6473c347b3a72719593976e1d52cb68dc6ba
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-10-15 14:05:15 -0400

    deploy to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	has_many :microposts
 	validates :name, length: { maximum: 20 }, presence: true
-	validates :email, length: { maximum: 30 }, presence: true 
+	validates :email, length: { maximum: 30 }, presence: true
 end
